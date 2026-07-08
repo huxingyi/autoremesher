2306b0c743f75099252fa43f8f91d3db88f5bb63
ShiroKSH <kushidashiro@gmail.com>
2026-07-09 01:08:27 +0300

    fix: validate obj io and render inputs

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -72,8 +72,8 @@ public:
         double sharpEdgeDegrees, double smoothNormalDegrees,
         double adaptivity,
         double anisotropy);
-    void runHeadless();
-    void saveMeshToFile(const QString& filename);
+    bool runHeadless();
+    bool saveMeshToFile(const QString& filename);
 
 signals:
     void headlessFinished(size_t quadCount, size_t nonQuadCount, size_t vertexCount, double elapsedSeconds);
